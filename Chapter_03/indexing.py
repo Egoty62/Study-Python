3edca841a9e0a190cb52934c39b10205b3172008
unknown <xodud602@gmail.com>
2023-01-17 19:12:35 +0900

    chapter_03_study

---
+++ Chapter_03/indexing.py
@@ -0,0 +1,4 @@
+colors = ['red', 'blue', 'green']
+print(colors[0])
+print(colors[2])
+print(len(colors)) # len() : 리스트 안에 있는 값의 개수를 반환
---
+++ Chapter_03/list_calculate.py
@@ -0,0 +1,6 @@
+## 덧셈연산
+color1 = ['red', 'green', 'blue']
+color2 = ['orange', 'black', 'white']
+print(color1 + color2) # 실제로 color1과 color2라는 변수에 영향을 주지는 않음
+print(len(color1))
+print(len(color1 + color2))
